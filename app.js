909ba046a61cc5b8619d284f0029b992fad543c2
Nico <bua-nico@hotmail.de>
2020-09-15 20:42:11 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const slides = document.getElementsByClassName("carousel-item");
+let slidePosition = 0;
+const totalSlides = slides.length;
+const dots = document.getElementsByClassName("dot");
+const dotContainer = document.querySelector(".dots");
+const timer = 5000;
+
+document
+    .getElementById("carousel-button-next")
+    .addEventListener("click", moveToNextSlide);
+document
+    .getElementById("carousel-button-prev")
+    .addEventListener("click", moveToPrevSlide);
+createDots();
+
+function hideAllSlides() {
+    for (let slide of slides) {
+        slide.classList.remove("carousel-item-visible");
+        slide.classList.add("carousel-item-hidden");
+    }
+}
+
+function moveToNextSlide() {
+    hideAllSlides();
+    hideDotActive();
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    slides[slidePosition].classList.add("carousel-item-visible");
+    dots[slidePosition].classList.add("dot-active");
+}
+
+function moveToPrevSlide() {
+    hideAllSlides();
+    hideDotActive();
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    slides[slidePosition].classList.add("carousel-item-visible");
+}
+
+function createDots() {
+    for (let i = 0; i < totalSlides; i++) {
+        const dot = document.createElement("div");
+        dot.classList.add("dot");
+        dotContainer.appendChild(dot);
+    }
+    dots[0].classList.add("dot-active");
+}
+
+function hideDotActive() {
+    for (const dot of dots) {
+        dot.classList.remove("dot-active");
+    }
+}
+
+let autoSlide = setInterval(moveToNextSlide, timer);
